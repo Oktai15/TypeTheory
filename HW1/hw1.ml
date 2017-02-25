a2b99da1c61e4b39b5692934679010267697b089
Oktai15 <oktai.tatanov1@gmail.com>
2017-02-25 15:32:18 +0300

    refactor, style and docs hw1.ml

--- HW1/hw1.ml
+++ HW1/hw1.ml
@@ -1,6 +1,11 @@
-(*open Genlex*)
-(* ocamlc -o peano hw1.mli hw1.ml t.ml *)
+(* This programm uses Genlex                       *)
+(* Write in console to run:                        *)
+(* > ocamlc -o <EXECUTOR-NAME> hw1.mli hw1.ml t.ml *)
+(* > ./<EXECUTOR-NAME>                             *)
+
 open Genlex;;
+
+(* Definition types for peano and lambda-expressions  *)
 
 type peano = Z | S of peano;;
 
@@ -8,6 +13,9 @@ type lambda =
 		| Var of string
 		| Abs of string * lambda
 		| App of lambda * lambda;;
+
+(* Operations for peano:                                 *)
+(* int_of_peano, peano_of_int, inc, add, mul, sub, power *)
 
 let rec int_of_peano x = match x with 
 	| Z -> 0
@@ -34,7 +42,6 @@ let rec sub x y = match (x,y) with
 	| (x, Z) -> x
 	| (S a, S b) -> sub a b;;
 
-
 let rec power x y = match (x,y) with 
 	| (x, Z) -> S Z 
 	| (Z, y) -> Z
@@ -44,95 +51,98 @@ let rec dop_rev x y = match (x,y) with
 	| (x, []) -> x
 	| (x, h::ys) -> dop_rev (h::x) ys;;
 
+(* Functions for list:                                       *)
+(* rev, dop_rev, merge, split_at, merge_sort, print_list_int *)
+
 let rev x = match x with 
 	| [] -> []
 	| x -> dop_rev [] x;; 
 
 let rec merge xs_1 xs_2 =
-	match (xs_1, xs_2) with 
-	| ([], xs_2) -> xs_2
-	| (xs_1, []) -> xs_1
-	| (x::s_1, y::s_2) ->
-	         if x>y then y::(merge xs_1 s_2) else x::(merge s_1 xs_2);;
+    match (xs_1, xs_2) with 
+    | ([], xs_2) -> xs_2
+    | (xs_1, []) -> xs_1
+    | (x::s_1, y::s_2) -> if x>y then y::(merge xs_1 s_2) else x::(merge s_1 xs_2);;
 
-let rec split_at mid l = 
-	match mid with 
-	| 0 -> ([], l)
-	| _ -> match l with
-		   | [] -> failwith "How?!" (*NEVER*) 
-		   | (x::xs) -> let res = split_at (mid-1) xs in 
-		   match res with 
-		   | (lr, rr) -> (x::lr, rr);;
+(* Examples:                           *)
+(* split_at 1 [4;5]   = ([4],[5])      *)
+(* split_at 1 [4;5;6] = ([4],[5,6])    *) 
+let rec split_at i l = 
+    match i with 
+    | 0 -> ([], l)
+    | _ -> (match l with
+            | [] -> failwith "Empty list" 
+            | (x::xs) -> 
+                    let res = split_at (i-1) xs in 
+		    match res with 
+		    | (lr, rr) -> (x::lr, rr));;
 
 let rec merge_sort x = 
-	let
-	  sz = List.length x
-	in 
-	let 
-	  halves = split_at (sz/2) x
-	in
-	match halves with
+    let sz = List.length x in 
+    let halves = split_at (sz/2) x in
+        match halves with
 	| _ when (sz = 1 || sz = 0) -> x
 	| (half_l, half_r) -> merge (merge_sort half_l) (merge_sort half_r);;
                      
-let rec print_int_list x = match x with 
-	| [] -> print_string " "
-	| a::xs -> print_int a; print_string " "; print_int_list xs;;
+let rec print_int_list x = 
+    match x with 
+    | [] -> print_string " "
+    | a::xs -> print_int a; print_string " "; print_int_list xs;;
+
+(* Functions for parser lambda-expressions:                   *)
+(* string_of_lambda, parse_lambda_of_tokens, lambda_of_string *) 
 
 let end_sym = ";";;
-let end_token_sym = Kwd ";";;
+let end_token_sym = Kwd ";";; (* No use *)
+let lexer_lambda = make_lexer ["\\";".";"(";")";";"];;
+let str_with_end_sym str = str^end_sym;;
 
 let rec string_of_lambda x =
- match x with
-	| Var l1 -> l1
-	| App (a,b) -> "(" ^ string_of_lambda a ^ " " ^ string_of_lambda b ^ ")"
-	| Abs (s,e) -> "(\\" ^ s ^ "." ^ string_of_lambda e ^ ")";; 
-
-
-let lexer_lambda = make_lexer ["\\";".";"(";")";";"];;
+    match x with
+    | Var l1 -> l1
+    | App (a,b) -> "(" ^ string_of_lambda a ^ " " ^ string_of_lambda b ^ ")"
+    | Abs (s,e) -> "(\\" ^ s ^ "." ^ string_of_lambda e ^ ")";; 
 
 let parse_lambda_of_tokens str_tokens =
-	let
-	  tn() = Stream.next str_tokens
-    in
-    let
-	  pk() = Stream.peek str_tokens
-    in
-    let 
-      cb() = if (tn() <> Kwd ")") then failwith "Unexpected symbol" 
-	in
-	let 
-      comm() = if (tn() <> Kwd ".") then failwith "Unexpected symbol" 
-	in
-	let rec parse_lambda() =
-		match (tn()) with
-            | Kwd "("  -> let pl = parse_lambda() in
+    let tn() = Stream.next str_tokens in
+    let pk() = Stream.peek str_tokens in
+    let cb() = if (tn() <> Kwd ")") then failwith "Unexpected symbol" in
+    let comm() = if (tn() <> Kwd ".") then failwith "Unexpected symbol" in
+    
+    let rec parse_lambda() =
+	match (tn()) with
+        | Kwd "("  -> 
+                let pl = parse_lambda() in
             	cb();
                 maybe_app pl;
 
-            | Kwd "\\" -> let pa = parse_abs() in
-     			maybe_app pa;
+        | Kwd "\\" ->
+                let pa = parse_abs() in
+     		maybe_app pa;
 
-            | Ident s  -> let v = Var s in
-     			maybe_app v;
-
-            | _ -> failwith "Unexpected symbol"
+        | Ident s  ->
+                let v = Var s in
+     		maybe_app v;
+                   
+        | _ -> failwith "Unexpected symbol"
 
     and parse_abs() = 
         match (tn()) with 
-            | Ident s -> 
-         		comm();
-    			Abs(s, parse_lambda());
-    		| _ -> failwith "Unexpected symbol" 
+        | Ident s -> 
+                comm();
+                Abs(s, parse_lambda());
+    	    
+        | _ -> failwith "Unexpected symbol" 
 
     and maybe_app l_app = 
         match (pk()) with 
-        | None -> failwith "Unexpected error";
-        | Some k -> match k with 
-        				| Kwd ")" -> l_app
-        				| Kwd ";" -> l_app
-        				| _ -> App(l_app, parse_lambda()) in
+        | None   -> failwith "Unexpected error";
+        | Some k -> 
+                (match k with 
+                 | Kwd ")" -> l_app
+      		 | Kwd ";" -> l_app
+        	 | _ -> App(l_app, parse_lambda())) in
 
     parse_lambda();; 
-let str_with_end_sym str = str^end_sym;; 
+ 
 let lambda_of_string x = parse_lambda_of_tokens (lexer_lambda(Stream.of_string (str_with_end_sym x)));;
